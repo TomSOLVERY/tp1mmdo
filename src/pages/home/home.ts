483660cdeeb823538bc210962e63b307ac119b38
Unknown <solverytom@hotmail.fr>
2018-04-11 14:56:40 +0200

    Requetes TMDB (fini)

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,9 +3,8 @@ import { NavController } from 'ionic-angular';
 import { DetailsPage } from '../details/details';
 import { Observable } from 'rxjs/Observable';
 import { HttpClient } from '@angular/common/http';
-import { HttpClientModule } from '@angular/common/http/src/module';
+import { DatePipe } from '@angular/common';
 import { key } from '../../app/tmdb';
-import { HttpParams } from '@angular/common/http/src/params';
 
 export interface Movie {
   title: string;
@@ -16,12 +15,6 @@ export interface Movie {
 
 }
 
-const film: Movie[] = [
-  { title: 'Seigneurs des Anneaux',  release_date: '2010', poster_path: 'https://image.tmdb.org/t/p/w185_and_h278_bestv2/1rF04Kk1eunZHk9OHPFwv8hllRF.jpg',vote_average: "9",overview:"Il était une fois" },
-  { title: 'Zootopie', release_date: '2017', poster_path: 'https://image.tmdb.org/t/p/w185_and_h278_bestv2/7pESsPdnttINTOxJAmfOOk9URO5.jpg',vote_average: "9",overview:"Il était une fois" },
-  { title: 'Thor',  release_date: '2018', poster_path: 'https://image.tmdb.org/t/p/w185_and_h278_bestv2/pNLGDvsJyTRdjVUJdc4iy6BQYK5.jpg',vote_average: "9",overview:"Il était une fois" }
-]
-
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -29,31 +22,30 @@ const film: Movie[] = [
 export class HomePage {
 
   result: Observable<Movie[]>;
-  picture: string;
-  resultempty : boolean;
   detailspage : any;
 
-
-
   constructor(public navCtrl: NavController,public http:HttpClient) {
-    this.resultempty = true;
     this.detailspage = DetailsPage;
     this.result = Observable.of([]);
   }
 
   onClear(ev: any) {
     this.result = Observable.of([]);
-    return;
   }
 
   fetchResults(query: string):Observable<Movie[]>{
-    query = query.trim();
-    return this.http.get<Movie[]>("https://api.themoviedb.org/3/search/movie?api_key="+key+"&language=fr-FR&query="+query+"&page=1&include_adult=false").pluck('results');
+    return this.http.get<Movie[]>("https://api.themoviedb.org/3/search/movie", {
+      params: {
+        query: query.trim(),
+        api_key: key,
+        language: 'fr-FR',
+        include_adult: 'false'
+      }
+    }).pluck('results');
   }
 
   getItems(ev: any) {
 
-    this.onClear(ev);
     let val = ev.target.value;
     
     if (val && val.trim() != '') {
